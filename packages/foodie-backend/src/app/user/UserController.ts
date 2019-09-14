4d050d86ef6c86e8ad73cbdb2a4f1c16aff28019
melitus <asmelitus@gmail.com>
2019-09-14 15:29:47 +0100

    fix merge error

--- packages/foodie-backend/src/app/user/UserController.ts
+++ packages/foodie-backend/src/app/user/UserController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { COOKIE_NAME, COOKIE_OPTIONS } from '../../middlewares/auth.middleware';
 import { Failure, Success } from '../ResponseHandlers';
 import * as UserActions from './UserActions';
 
@@ -8,7 +7,6 @@ export function GetUsers(req: Request, res: Response) {
   UserActions.GetUsers()
     .then(result => Success(res, result, 'users fetched'))
     .catch(error => Failure(res, error, 'error in fetching users'));
-
 }
 
 export function GetUser(req: Request, res: Response) {
@@ -17,16 +15,4 @@ export function GetUser(req: Request, res: Response) {
   UserActions.GetUser(id)
     .then(result => Success(res, result, 'user fetched'))
     .catch(error => Failure(res, error, 'error in fetching user'));
-
-}
-
-export function SignUpUser(req: Request, res: Response) {
-
-  UserActions.SignUpUser(req.body)
-    .then(result => {
-      res.cookie(COOKIE_NAME, result.token, COOKIE_OPTIONS);
-      Success(res, result, 'user registered');
-    })
-    .catch(error => Failure(res, error, 'error in registering user'));
-
 }
